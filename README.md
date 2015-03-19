46883b9a09986ad9e6abde06d363af68733f24f0
chengkai liang <ckm.liang@gmail.com>
2015-03-19 13:08:20 -0700

    initial keychian cookbook checkin

---
+++ README.md
@@ -0,0 +1,4 @@
+# keychain
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'keychain'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures keychain'
+long_description 'Installs/Configures keychain'
+version          '0.1.0'
+
