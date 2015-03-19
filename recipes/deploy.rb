a0f021be388a6d341b79a59878f6e2007558108b
chengkai liang <ckm.liang@gmail.com>
2015-03-19 14:30:33 -0700

    add code to make keychain ready

--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -4,4 +4,10 @@ git_app_workspace = File.join( file_cache_path, 'keychain' )
 git git_app_workspace do
   repository node['keychain']['git']['repo']
   action :sync
+  notifies :run, 'execute[make-keychain]', :immediately
+end
+
+execute 'make-keychain' do
+  cwd git_app_workspace
+  command 'make'
 end
