87c8eed42ea6dc2b7114b207e9be744fe46e1f24
chengkai liang <ckm.liang@gmail.com>
2015-03-19 14:40:23 -0700

    add code to install keychain script

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,1 +1,3 @@
 default['keychain']['git']['repo'] = 'https://github.com/funtoo/keychain.git'
+default['keychain']['install_root'] = '/usr/local/bin'
+default['keychain']['manpath'] = '/usr/local/share/man1'
--- recipes/deploy.rb
+++ recipes/deploy.rb
@@ -5,9 +5,16 @@ git git_app_workspace do
   repository node['keychain']['git']['repo']
   action :sync
   notifies :run, 'execute[make-keychain]', :immediately
+  notifies :run, 'execute[install-keychain-script]', :immediately
 end
 
 execute 'make-keychain' do
   cwd git_app_workspace
   command 'make'
+end
+
+execute 'install-keychain-script' do
+  cwd git_app_workspace
+  command "cp keychain.sh #{node['keychain']['install_root']}/keychain"
+  not_if "test -e #{node['keychain']['install_root']}/keychain"
 end
