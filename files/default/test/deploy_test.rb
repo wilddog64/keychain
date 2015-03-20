47eadaed05b235088085ce1edb79576987fd7ec8
chengkai liang <ckm.liang@gmail.com>
2015-03-19 18:01:38 -0700

    make sentence more clear

--- files/default/test/deploy_test.rb
+++ files/default/test/deploy_test.rb
@@ -1,5 +1,5 @@
 describe_recipe 'keychain_git_repo_deployment' do
-  it 'should create git repo' do
+  it 'should create keychain git repo' do
     keychain_workspace = File.join( Chef::Config[:file_cache_path], 'keychain' )
     directory( keychain_workspace ).must_exist.with( :owner, 'root' )
     assert_directory( keychain_workspace, 'root', 'root', '755' )
