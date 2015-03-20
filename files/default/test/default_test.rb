90750dd26d4455e199dc803f0ddb0d95e0545e05
chengkai liang <ckm.liang@gmail.com>
2015-03-19 17:54:33 -0700

    test cases for ensuring keychain man page are deployed and executable

--- files/default/test/default_test.rb
+++ files/default/test/default_test.rb
@@ -8,4 +8,14 @@ describe_recipe 'keychain_git_repo_deployment' do
   it 'make keychain script executable' do
     assert_sh( 'keychain -V', '2.7.2_beta1' )
   end
+
+  it 'should copy keychain man page' do
+    keychain_man_page_location = File.join( node['keychain']['manpath'], 'keychain.1.gz' )
+    file( keychain_man_page_location ).must_exist.with( :owner, 'root' ).and( :user, 'root' )
+    assert_file( keychain_man_page_location, 'root', 'root', '644' )
+  end
+
+  it 'should display keychain man page' do
+    assert_sh( 'man -P cat keychain', '2.7.2_beta1' )
+  end
 end
